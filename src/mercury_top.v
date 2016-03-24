3d3d781ba074f6195830ae6fba276614488ab81e
Ubuntu <ubuntu@ip-172-31-60-191.ec2.internal>
2016-03-24 10:46:13 +0000

    Adding DCM

--- src/mercury_top.v
+++ src/mercury_top.v
@@ -30,10 +30,13 @@ module mercury_top
  input  wire         PS2_CLK,
 );
 
+// hang reset off of pll_locked and button pushes
+wire  pll_reset;
 // DCM outputs
 wire  app_clk100, app_arst100_n;
 wire  app_clk25,  app_arst25_n;
 wire  app_clk50,  app_arst50_n;
+wire  pll_locked;
 
 // VGA output
 wire          vsync_out;
@@ -42,10 +45,21 @@ wire [02:00]  red_out;
 wire [02:00]  green_out;
 wire [01:00]  blue_out;
 
-
 // Xilinx DCM
-
-
+assign pll_reset = SW[0];
+mercury_dcm merc_dcm
+(
+  .CLKIN_IN            (CLK),
+  .RST_IN              (pll_reset), 
+  .CLKDV_OUT           (app_clk25), 
+  .CLKIN_IBUFG_OUT     (app_clk50), 
+  .CLK0_OUT            (), 
+  .CLK2X_OUT           (app_clk100), 
+  .LOCKED_OUT          (pll_locked)
+);
+assign app_arst100_n = ~pll_locked;
+assign app_arst50_n = ~pll_locked;
+assign app_arst25_n = ~pll_locked;
 // lets throw a VGA thing into this
 vga_sync vga
 (
